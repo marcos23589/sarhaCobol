40f2d6911d0f179e65354c45c0193dfff0b1519f
marcos23589 <marcos23589@gmail.com>
2023-06-27 16:00:58 -0300

    [refactor]

--- src/helpers/fecha.js
+++ src/helpers/fecha.js
@@ -1,29 +1,19 @@
+const today = new Date();
+const ultimoDia = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0');
+const yy = today.getFullYear().toString().slice(2, 4);
+const yyyy = today.getFullYear();
 
-    let today = new Date();
-    let ultimoDia = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-    let hoy
+const fechaDesde = `01${mm}${yyyy}`;
+const periodoDesde = `${yyyy}${mm}`;
+const fechaHasta = `${ultimoDia.getDate()}${mm}${yyyy}`;
 
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yy = today.getFullYear().toString().slice(2,4)
-    let yyyy = today.getFullYear();
-
-    let fechaDesde = `01${mm+yyyy}`
-    let periodoDesde = `${yyyy+mm}` 
-    let fechaHasta = `${ultimoDia.getDate()+mm+yyyy}`
-
-    today = dd + '/' + mm + '/' + yyyy;
-    hoy = dd + mm + yy
-
+const hoy = dd + mm + yy;
 
 module.exports = {
-    hoy,
-    fechaDesde,
-    periodoDesde,
-    fechaHasta
-}
-
-
-
-
-  
+  hoy,
+  fechaDesde,
+  periodoDesde,
+  fechaHasta
+};
--- src/helpers/salidas.js
+++ src/helpers/salidas.js
@@ -1,37 +1,20 @@
 function generarStringSalidaParaAfiliacion(cuil, fechaDesde, afiliaciones) {
-    return "1" +
-      cuil.toString() +
-      fechaDesde +
-      afiliaciones.toString().padStart(8, "0") +
-      "0000000000";
+  return `1${cuil}${fechaDesde}${afiliaciones.toString().padStart(8, "0")}0000000000`;
+}
+
+function generarStringSalidaParaDescuento(cuil, cod_institucion, cod_descuento, importe, periodoDesde) {
+  return `${cuil}${cod_institucion.padStart(6, "0")}${cod_descuento.padStart(4, "0")}${(importe / 100).toString().padStart(11, "0")}${periodoDesde}${periodoDesde}`;
+}
+
+function generarStringSalida(cuil, CODIGO, subsarha, desde, periodoDesde, reintegro, hasta, cantidad, importe, afiliaciones, cod_institucion, cod_descuento, fechaDesde) {
+  if (afiliaciones !== -1) {
+    return generarStringSalidaParaAfiliacion(cuil, fechaDesde, afiliaciones);
+  } else if (cod_institucion) {
+    return generarStringSalidaParaDescuento(cuil, cod_institucion, cod_descuento, importe, periodoDesde);
+  } else {
+    //RELLENA LOS CAMPOS AL PRINCIPIO
+    return `${cuil}${CODIGO.toString().padStart(4, "0")}${subsarha.toString().padStart(4, "0")}${desde}${periodoDesde}${reintegro}${hasta}${cantidad.toString().padStart(8, "0").padEnd(14, "0")}${importe.toString().padStart(12, "0")}`;
   }
-  
-  function generarStringSalidaParaDescuento(cuil, cod_institucion, cod_descuento, importe, periodoDesde) {
-    return cuil.toString() +
-      cod_institucion.padStart(6, "0") +
-      cod_descuento.padStart(4, "0") +
-      (importe / 100).toString().padStart(11, "0") +
-      periodoDesde.toString() +
-      periodoDesde.toString();
-  }
-  
-  function generarStringSalida(cuil, CODIGO, subsarha, desde, periodoDesde, reintegro, hasta, cantidad, importe, afiliaciones, cod_institucion, cod_descuento, fechaDesde) {
-    if (afiliaciones != -1) {
-      return generarStringSalidaParaAfiliacion(cuil, fechaDesde, afiliaciones);
-    } else if (cod_institucion) {
-      return generarStringSalidaParaDescuento(cuil, cod_institucion, cod_descuento, importe, periodoDesde);
-    } else {
-      //RELLENA LOS CAMPOS AL PRINCIPIO
-      return cuil.toString() +
-        CODIGO.toString().padStart(4, "0") +
-        subsarha.toString().padStart(4, "0") +
-        desde +
-        periodoDesde +
-        reintegro +
-        hasta +
-        cantidad.toString().padStart(8, "0").padEnd(14, "0") +
-        importe.toString().padStart(12, "0");
-    }
-  }
-  
-  module.exports = generarStringSalida
+}
+
+module.exports = generarStringSalida;
