3b23dfe62092aa812467f51c5110eb6c18b0f740
Marcos Daniel Olivera <marcos.olivera@mefi.gob.ar>
2023-07-17 15:58:35 -0300

    [add+refactor]citec+mii

--- src/controllers/pages.controller.js
+++ src/controllers/pages.controller.js
@@ -1,78 +1,72 @@
-const Concepto = require('../models/conceptos')
-const liquidacion = require('../models/Liquidacion')
-const filtro = require('../helpers/conceptos')
-const Asignaciones = require('../models/Asignaciones')
-const {sumaCodigos} = require('../helpers/filtros')
+const Concepto = require("../models/conceptos");
+const liquidacion = require("../models/Liquidacion");
+const filtro = require("../helpers/conceptos");
+const Asignaciones = require("../models/Asignaciones");
+const { sumaCodigos } = require("../helpers/filtros");
 
 exports.home = (req, res) => {
-    return res.render("home", {titulo: "Sarha-Cobol"})
-}
+  return res.render("home", { titulo: "Sarha-Cobol" });
+};
 
-exports.getLiquidacion =   (req, res) => {    
-    return res.render("liquidacion", {titulo: "Liquidacion"})
-}
+exports.getLiquidacion = (req, res) => {
+  return res.render("liquidacion", { titulo: "Liquidacion" });
+};
 
 exports.getEsidif = (req, res) => {
- return res.render('esidif', {titulo: "e-SIDIF"})                                                                                                                                         
-}
+  return res.render("esidif", { titulo: "e-SIDIF" });
+};
 
 exports.getPreconceptos = async (req, res) => {
-    try {
-      const cantidad = await liquidacion.countDocuments();
-      const resultado = await Concepto.find();
-      const tamano = resultado.length;
-      const res1 = resultado.slice(0, tamano * (1 / 3));
-      const res2 = resultado.slice(tamano * (1 / 3), tamano * (2 / 3));
-      const res3 = resultado.slice(tamano * (2 / 3));
-      
-      return res.render("preconceptos", {
-        res1,
-        res2,
-        res3,
-        titulo: "Preconceptos",
-        cantidad,
-      });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).send("Ha ocurrido un error");
-    }
-  };
-  
+  try {
+    const cantidad = await liquidacion.countDocuments();
+    const resultado = await Concepto.find();
+    const tamano = resultado.length;
+    const res1 = resultado.slice(0, tamano * (1 / 3));
+    const res2 = resultado.slice(tamano * (1 / 3), tamano * (2 / 3));
+    const res3 = resultado.slice(tamano * (2 / 3));
+
+    return res.render("preconceptos", {
+      res1,
+      res2,
+      res3,
+      titulo: "Preconceptos",
+      cantidad,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Ha ocurrido un error");
+  }
+};
 
 exports.postPreconceptos = async (req, res) => {
-    
-    const preconceptos = req.body.cobol
+  const preconceptos = req.body.cobol;
 
-    //SE OBTIENEN LOS CONCEPTOS A GENERAR TXTS
-    let archivo = await liquidacion.find({ "CODIGO" : preconceptos})
-    
-    //SUMA CODIGOS 250-280 y 930-933(COBOL)    
-    sumaCodigos(archivo)
+  //SE OBTIENEN LOS CONCEPTOS A GENERAR TXTS
+  let archivo = await liquidacion.find({ CODIGO: preconceptos });
 
-    for (let i = 0; i < archivo.length; i++) {
-        let importe = Math.ceil(archivo[i].IMPORTE * 100);        
-        let auxSarha = await Concepto.find({"cobol": archivo[i].CODIGO})
-        let cantidad = -1;
-        let aux =  await Asignaciones.findOne({"SARHA": auxSarha[0].sarha})        
-        
-        if(aux){            
-          //redondea al entero más cercano
-          cantidad = Math.round(importe / (aux.MONTO*100));
-                                   
-        }
-        
-        //FILTRO PARA CREAR LOS ARCHIVOS
-        filtro(
-            auxSarha[0].sarha,
-            importe, 
-            archivo[i].CUIL, 
-            cantidad, 
-            auxSarha[0].denominacion,
-            auxSarha[0].subsarha)
-    }  
-    
-    return res.redirect("preconceptos")
-}
+  //SUMA CODIGOS 250-280 y 930-933(COBOL)
+  sumaCodigos(archivo);
 
+  for (let i = 0; i < archivo.length; i++) {
+    let importe = Math.ceil(archivo[i].IMPORTE * 100);
+    let auxSarha = await Concepto.find({ cobol: archivo[i].CODIGO });
+    let cantidad = -1;
+    let aux = await Asignaciones.findOne({ SARHA: auxSarha[0].sarha });
+    if (aux) {
+      //redondea al entero más cercano
+      cantidad = Math.round(importe / (aux.MONTO * 100));
+    }
 
+    //FILTRO PARA CREAR LOS ARCHIVOS
+    filtro(
+      auxSarha[0].sarha,
+      importe,
+      archivo[i].CUIL,
+      cantidad,
+      auxSarha[0].denominacion,
+      auxSarha[0].subsarha
+    );
+  }
 
+  return res.redirect("preconceptos");
+};
--- src/server.js
+++ src/server.js
@@ -1,47 +1,51 @@
-const express = require('express')
-const morgan = require('morgan')
-const router = require('./routes/index.routes')
-const path = require('path')
-const { engine } = require('express-handlebars');
-const Handlebars = require('handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const express = require("express");
+const morgan = require("morgan");
+const router = require("./routes/index.routes");
+const path = require("path");
+const { engine } = require("express-handlebars");
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 
 //inicializadores
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
 //BBDD
-require('./config/database')
+require("./config/database");
 
-//SETTINGS  
-app.set("views", path.join(__dirname, "views"))
-app.engine('.hbs', engine({
-    defaultLayout: 'main',
+//SETTINGS
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  engine({
+    defaultLayout: "main",
     layoutsDir: path.join(app.get("views"), "layouts"),
     partialsDir: path.join(app.get("views"), "partials"),
     extname: ".hbs",
-    handlebars: allowInsecurePrototypeAccess(Handlebars)
-    //helpers: require("./lib/handlebars")    
-}))
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    //helpers: require("./lib/handlebars")
+  })
+);
 
 app.set("view engine", ".hbs");
 
 //middlewares
-app.use(morgan("dev"))
-
+app.use(morgan("dev"));
 
 //PUBLIC
-app.use(express.static(path.join(__dirname, './views/public')));
+app.use(express.static(path.join(__dirname, "./views/public")));
 
 //archivos estáticos
-app.use('/src/views/public', express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use("/src/views/public", express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //rutas
-app.use('/', router)
+app.use("/", router);
 
 //servidor
-app.listen(PORT, ()=> {
-    console.log("Server escuchando en puerto",PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server escuchando en puerto", PORT);
+});
